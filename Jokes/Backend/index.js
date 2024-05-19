16aabe86f585a691526d462df348eb7f25678b42
saurvkmr0 <saurvkmr0@gmail.com>
2024-05-19 23:49:08 +0530

    starting to build a simple joke app

---
+++ Jokes/Backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app=express();
+require('dotenv').config();
+
+app.get('/jokes',(req,res) => {
+    const jokes = [
+        {
+            id:1,
+            title:"joke1",
+            discp:"this is the first joke",
+        },
+        {
+            id:2,
+            title:"joke2",
+            discp:"this is the second joke",
+        },
+        {
+            id:3,
+            title:"joke3",
+            discp:"this is the third joke",
+        },
+        {
+            id:4,
+            title:"joke4",
+            discp:"this is the fourth joke",
+        },
+        {
+            id:5,
+            title:"joke5",
+            discp:"this is the fifth joke",
+        },
+    ]
+    res.send(jokes)
+})
+
+app.get('/',(req,res) => {
+    res.send('<h1>Welcome to jokes page</h1>')
+})
+
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
